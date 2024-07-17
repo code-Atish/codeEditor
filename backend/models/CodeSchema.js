fc36b44a9032c8d772de0d5bf0cd657c0ed2c498
Chergui CHihqb Iddine <chihab.chergui.1991@gmail.com>
2024-07-17 17:51:44 +0100

    add code schema

--- backend/models/CodeSchema.js
+++ backend/models/CodeSchema.js
@@ -1,1 +1,41 @@
-// contains code schema
+const mongoose = require("mongoose");
+
+const sampleInputOutputSchema = mongoose.Schema({
+	input: {
+		type: String,
+		required: true,
+	},
+	output: {
+		type: String,
+		required: true,
+	},
+});
+const codeSchema = mongoose.Schema(
+	{
+		objective: {
+			type: String,
+			required: true,
+		},
+		task: {
+			type: String,
+			required: true,
+		},
+		inputFormat: {
+			type: String,
+			required: true,
+		},
+		outputFormat: {
+			type: String,
+			required: true,
+		},
+		sampleInputOutput: {
+			type: [sampleInputOutputSchema],
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("Code", codeSchema);
